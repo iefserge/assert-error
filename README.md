42dbbfdbe44f171c13371daf6c13bffb0aba3dcd
Serge <iefserge@runtimejs.org>
2015-05-02 19:32:41 +0300

    First commit

---
+++ README.md
@@ -0,0 +1,14 @@
+## SYNOPSIS
+
+Simple assert function that throws provided error object.
+
+## USAGE
+
+```js
+var assert = require('assert-error');
+assert(true === true, new Error('my error'));
+```
+
+##LICENSE
+
+Apache License, Version 2.0
---
+++ test.js
@@ -0,0 +1,66 @@
+// Copyright 2015 Sergii Iefremov
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+var test = require('tape');
+var assert = require('./');
+
+test('assert using correct error object', function(t) {
+  var error = new Error();
+  error.code = 200;
+
+  t.doesNotThrow(function() {
+    assert(true, error);
+  });
+
+  t.throws(function() {
+    assert(false, error);
+  });
+
+  t.throws(function() {
+    assert(void 0, error);
+  });
+
+  t.throws(function() {
+    assert(0, error);
+  });
+
+  t.end();
+});
+
+test('should throw when there are no error object', function(t) {
+  var error = new Error();
+  error.code = 404;
+
+  t.throws(function() {
+    try {
+      assert(true);
+    } catch (e) {
+      t.ok(e);
+      t.equal(e.message, 'unknown assert error');
+      throw e;
+    }
+  });
+
+  t.throws(function() {
+    try {
+      assert(false);
+    } catch (e) {
+      t.ok(e);
+      t.equal(e.message, 'unknown assert error');
+      throw e;
+    }
+  });
+
+  t.end();
+});
