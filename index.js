4bd88c7580b4db874ae3703f249c8e02f2b881ec
Serge <iefserge@runtimejs.org>
2015-06-07 21:06:47 +0300

    Strict mode

--- index.js
+++ index.js
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+'use strict';
 module.exports = function(value, error) {
   if (!error) {
     throw new Error('error required');
